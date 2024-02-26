304c58d6be3cdb6dabcb4cc85c1cc50ad8d7e1a6
alonshlomi1 <alonasshlomi@gmail.com>
2024-02-26 20:41:06 +0200

    fix coin

--- app/src/main/java/com/example/spaceexplorerandroidapp/Model/Coin.java
+++ app/src/main/java/com/example/spaceexplorerandroidapp/Model/Coin.java
@@ -3,7 +3,7 @@ package com.example.spaceexplorerandroidapp.Model;
 import com.example.spaceexplorerandroidapp.R;
 
 public class Coin extends GameObject{
-    private static final int src = R.drawable.coin3;
+    private static final int src = R.drawable.coin2;
     private static final int COIN_POINTS = 15;
 
     public Coin(){
